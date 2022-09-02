abd9bcd720e02522ea560285546bb3480d87cd41
udashsanam <udashsanam@gmail.com>
2022-09-02 08:13:59 +0545

    add: first components

--- complete-react-guide/src/App.js
+++ complete-react-guide/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItems';
 
 function App() {
   return (
     <div className="App">
      <h1> Lets learn react </h1>
+     <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ complete-react-guide/src/components/ExpenseItems.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div> 2022 -09-02</div>
+      <div>
+        <h1> Food </h1>
+      </div>
+      <div>  Rs 200 </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;

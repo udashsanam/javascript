4f9ae386abea1a63eb0bb92d4cd3143682dcebdb
udashsanam <udashsanam@gmail.com>
2022-09-03 16:06:06 +0545

    add: seperated the date componenet

--- complete-react-guide/src/components/ExpenseItems.js
+++ complete-react-guide/src/components/ExpenseItems.js
@@ -1,11 +1,13 @@
 import './ExpenseItem.css';
+import ExpenseDate  from './ExpenseDate';
 function ExpenseItem(props) {
   // const expenseDate= new Date(2022, 2, 14);
   // const expenseTitle = 'Food';
   // const expenseAmount = 234.45;
   return (
     <div className= "expense-item">
-      <div>   {(props.date).toISOString()} </div>
+      {/* <div>   {(props.date).toISOString()} </div> */}
+      <ExpenseDate date = {props.date} > </ExpenseDate>
       <div className= "expense-item__description">
         <h2> {props.title}</h2>
         <div className="expense-item__price">  Rs {props.amount} </div>

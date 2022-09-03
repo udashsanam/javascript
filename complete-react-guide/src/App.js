7b35d2f04c7f62929b22cc3a50f821a0a0041b0f
udashsanam <udashsanam@gmail.com>
2022-09-03 17:04:43 +0545

    add: extracting another componenst

--- complete-react-guide/src/App.js
+++ complete-react-guide/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItems';
+// import ExpenseItem from './components/ExpenseItems';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -27,14 +28,9 @@ function App() {
 
   return (
      
-
-    <div className="App">
+    <div >
      <h1> Lets learn react </h1>
-     <ExpenseItem title={expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date}></ExpenseItem>
-     <ExpenseItem title={expenses[1].title} amount = {expenses[1].amount} date = {expenses[1].date}></ExpenseItem>
-     <ExpenseItem title={expenses[2].title} amount = {expenses[2].amount} date = {expenses[2].date}></ExpenseItem>
-     <ExpenseItem title={expenses[3].title} amount = {expenses[3].amount} date = {expenses[3].date}></ExpenseItem>
-
+    <Expenses items={expenses}></Expenses>
     </div>
   );
 }

d53f71b2d5f09d6e03dcd5ee9b36ce1c070e551b
udashsanam <udashsanam@gmail.com>
2022-09-04 11:13:31 +0545

    useState Learned

--- complete-react-guide/src/components/Expenses/ExpenseItems.js
+++ complete-react-guide/src/components/Expenses/ExpenseItems.js
@@ -1,19 +1,35 @@
 import './ExpenseItem.css';
 import ExpenseDate  from './ExpenseDate';
 import Card from '../UI/Card';
+import {useState} from 'react';
 const ExpenseItem = (props) => {
   // const expenseDate= new Date(2022, 2, 14);
   // const expenseTitle = 'Food';
   // const expenseAmount = 234.45;
+
+
+  const [title, setTitle] = useState(props.title);
+
+  // eventhaldler funciton 
+  const clickHandler = () => {
+
+    // this will call the jsx again and change the value to the variable into the react 
+    setTitle('update');
+  
+    console.log(title);
+  }
+
   return (
     <Card className= "expense-item">
       {/* <div>   {(props.date).toISOString()} </div> */}
       <ExpenseDate date = {props.date} > </ExpenseDate>
       <div className= "expense-item__description">
-        <h2> {props.title}</h2>
+        <h2> {title}</h2>
         {/* we are using reusable component card for css here also  */}
         <Card className="expense-item__price">  Rs {props.amount} </Card>
       </div>
+      <button onClick={clickHandler}> change title </button>
+
     </Card>
   );
 }

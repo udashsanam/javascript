79af534eb7af92780615d4585897230a15290b92
udashsanam <udashsanam@gmail.com>
2022-09-16 22:14:34 +0545

    completed the expense calculator

--- complete-react-guide/src/components/Expenses/Expenses.js
+++ complete-react-guide/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 // taking the expenses as same name as passing props from the parent componen
 const Expenses = ({ expenses }) => {
@@ -17,13 +18,13 @@ const Expenses = ({ expenses }) => {
   });
 
 
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selectedYear={selectedYear}
         onChangeFilter={onChangeHandler}
       />
+      <ExpensesChart expenses = {filteredExpenses}/>
       
       <ExpensesList expenses = {filteredExpenses} />
     </Card>
